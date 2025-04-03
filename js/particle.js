c9128648cd118a1219fc1b5b9396b15e46340605
thuanbui1309 <buiminhthuan1314@gmail.com>
2025-04-03 13:08:46 +0700

    add heart

--- js/particle.js
+++ js/particle.js
@@ -3,11 +3,11 @@
  */
 var settings = {
   particles: {
-    length: 2500, // maximum amount of particles
-    duration: 5, // particle duration in sec
+    length: 500, // maximum amount of particles
+    duration: 2, // particle duration in sec
     velocity: 100, // particle velocity in pixels/sec
     effect: -0.75, // play with this for a nice effect
-    size: 20, // particle size in pixels
+    size: 30, // particle size in pixels
   },
 };
 
